0f803f95f07d650bff120c460c30f0a0586d684e
davis mcpherson <davis.mcpherson@gmail.com>
2012-05-09 12:29:42 -0400

    demo app using postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PgApp::Application.config.secret_token = '6a7c8a6715d5c6a5a8a91d9bbeda60d205345ab2478c8d836f63473ed870df5e6107d2e1d37abfb48ea614085b9cb11418bc11b0a471acafa19996f2b2a1f280'
